e70edb1aa0678679f07381f054428d3bd084dde2
RomanczuG <romanczug@icloud.com>
2023-04-05 20:00:36 -0400

    first

---
+++ analyze_results.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+def analyze_results(file_path):
+    with open(file_path) as f:
+        lines = f.readlines()
+
+    data = {
+        'operation': [],
+        'latency': [],
+        'throughput': []
+    }
+
+    for line in lines:
+        if line.startswith("[READ]") or line.startswith("[UPDATE]"):
+            parts = line.strip().split()
+            data['operation'].append(parts[0][1:-1])
+            if parts[1] == "AverageLatency(us)":
+                data['latency'].append(float(parts[2]))
+            elif parts[1] == "Throughput(ops/sec)":
+                data['throughput'].append(float(parts[2]))
+
+    df = pd.DataFrame(data)
+    p95_latency = df['latency'].quantile(0.95)
+    avg_throughput = df['throughput'].mean()
+
+    print("P95 Latency (us):", p95_latency)
+    print("Average Throughput (ops/sec):", avg_throughput)
+
+if __name__ == "__main__":
+    analyze_results("results.txt")
